0d98450996f4a07aeebd3ac92a8dfc9a13d6b668
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2014-09-22 16:13:38 +0200

    Now delegating to existing twitter-streaming-client.
    Upgraded some deps. Changed to logback.

--- twitter-producer/project.clj
+++ twitter-producer/project.clj
@@ -4,12 +4,26 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [twitter-api "0.7.6"]
+                 [twitter-streaming-client "0.3.2"
+                  :exclusions [org.apache.httpcomponents/httpclient
+                               org.apache.httpcomponents/httpcore]]
                  [cheshire "5.3.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.7"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [amazonica "0.2.24"]
+
+                 ;; twitter-streaming-client wants 1.7.5 of these, no way dude
+                 [org.slf4j/slf4j-api "1.7.7"]
+                 [org.slf4j/jcl-over-slf4j "1.7.7"]
+                 [org.slf4j/jul-to-slf4j "1.7.7"]
+                 [org.slf4j/log4j-over-slf4j "1.7.7"]
+
+                 [org.clojure/tools.logging "0.3.1"]
+                 [amazonica "0.2.25"
+                  :exclusions [commons-codec joda-time]]
                  [clj-time "0.8.0"]
+
+                 ;; some deps want 2.2, but forget it
+                 [joda-time "2.3"]
+
                  [environ "1.0.0"]]
   :plugins [[lein-environ "1.0.0"]]
   :main ^:skip-aot twitter-producer.core
--- twitter-producer/src/twitter_producer/core.clj
+++ twitter-producer/src/twitter_producer/core.clj
@@ -1,36 +1,30 @@
 (ns twitter-producer.core
-  (:require [twitter.oauth :as oauth]
-            [twitter.callbacks :as callbacks]
-            [twitter.callbacks.handlers :as handlers]
-            [twitter.api.streaming :as streaming]
-            [cheshire.core :as json]
-            [clojure.tools.logging :refer [info error]]
+  (:require [twitter-streaming-client.core :as client]
+            [twitter.oauth :as oauth]
+            [twitter.api.streaming]
+            [clojure.tools.logging :refer [debug info error]]
             [amazonica.aws.kinesis :as kinesis]
             [amazonica.core :refer [defcredential]]
             [clj-time.format :as format]
             [clj-time.coerce :refer [to-date]]
             [environ.core :refer [env]])
-  (:import twitter.callbacks.protocols.AsyncStreamingCallback
-           com.amazonaws.services.kinesis.model.ResourceInUseException
+  (:import com.amazonaws.services.kinesis.model.ResourceInUseException
            java.util.Locale)
   (:gen-class))
 
-;; keep track of incomplete bodyparts
-(def body (atom nil))
-
-(def my-creds (oauth/make-oauth-creds (env :consumer-key)
-                                      (env :consumer-secret)
-                                      (env :access-token)
-                                      (env :access-token-secret)))
+(def twitter-creds (oauth/make-oauth-creds (env :consumer-key)
+                                           (env :consumer-secret)
+                                           (env :access-token)
+                                           (env :access-token-secret)))
 
 (defcredential (env :aws-access-key-id) (env :aws-secret-key) (env :aws-region))
 
-(def ^:dynamic *stream-name* "Twitter")
+(def ^:dynamic *kinesis-stream-name* "Twitter")
 
 (def ^:dynamic *kinesis-uri* "https://kinesis.eu-west-1.amazonaws.com")
 
 ;; make sure we have a working stream
-(defn create-stream [stream-name]
+(defn create-kinesis-stream [stream-name]
   (let [streams (kinesis/list-streams)
         stream-names (set (:stream-names streams))]
     (if (stream-names stream-name)
@@ -39,7 +33,9 @@
         (let [stream (kinesis/describe-stream stream-name)
               status (get-in stream [:stream-description :stream-status])]
           (case status
-            "ACTIVE" (info "Kinesis stream" stream-name "is active")
+            "ACTIVE" (do
+                       (info "Kinesis stream" stream-name "is active")
+                       stream)
             "CREATING" (do (info "Stream" stream-name "is in status CREATING, waiting until it's done")
                            (Thread/sleep 5000)
                            (recur stream-name))
@@ -51,7 +47,8 @@
         (try
           (kinesis/create-stream stream-name 1)
           (catch ResourceInUseException e
-            (error "Stream" stream-name "already exists, somebody got in between")))))))
+            (error "Stream" stream-name "already exists, somebody got in between")
+            (System/exit 2)))))))
 
 #_(kinesis/describe-stream "Twitter")
 
@@ -59,7 +56,7 @@
   (let [data {:created-at date
               :tag hashtag}]
     (info "Posting to Kinesis:" data)
-    (kinesis/put-record *stream-name*
+    (kinesis/put-record *kinesis-stream-name*
                         data
                         hashtag)))
 
@@ -76,38 +73,33 @@
     (doseq [hashtag (mapv :text hashtags)]
       (handle-hashtag date hashtag))))
 
-(defn on-bodypart
-  "A streaming on-bodypart handler.
-    baos is a ByteArrayOutputStream. (str baos) is the response body (encoded as JSON)."
-  [response baos]
-  (let [bodypart (str baos)]
-    (try
-      (swap! body str bodypart)
-      (handle-tweet (json/parse-string @body true))
-      (reset! body nil)
-      (catch Exception e
-        #_(info "Incomplete tweet, waiting for next chunk\n" bodypart)))))
+(def kinesis-stream (create-kinesis-stream *kinesis-stream-name*))
 
-(def async-streaming-callback
-  (AsyncStreamingCallback.
-   ;; our handler, called for each Tweet that comes in from the Streaming API.
-   on-bodypart
-   ;; return the Twitter API error message on failure
-   handlers/get-twitter-error-message
-   ;; just print exceptions to the console when there's an exception
-   handlers/exception-print))
+(def twitter-stream (client/create-twitter-stream
+                     twitter.api.streaming/statuses-sample
+                     :oauth-creds twitter-creds))
 
-(defn start []
-    (streaming/statuses-sample :oauth-creds my-creds
-                               :callbacks async-streaming-callback))
+(defn cleanup []
+  (info "Cancelling the Twitter stream")
+  (client/cancel-twitter-stream twitter-stream)
+  (info "Note that Kinesis stream" (-> kinesis-stream :stream-description :stream-arn)
+        "still exists"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [stream (create-stream *stream-name*)
-        data (meta stream)]
-
-    (try
-      (start)
-      (catch Exception e
-        ((:cancel data))))))
+  (info "Twitter stream is" ())
+  (client/start-twitter-stream twitter-stream)
+  (.addShutdownHook (Runtime/getRuntime) (Thread. cleanup))
+  (try
+    (loop []
+      (let [queues (client/retrieve-queues twitter-stream)
+            tweets (:tweet queues)]
+        (doseq [tweet tweets]
+          (debug "Handling tweet" (:id tweet))
+          (handle-tweet tweet))
+        (recur)))
+    (catch Exception e
+      (error "A problem occurred when retrieving tweets" (.getMessage e))
+      (cleanup)
+      (System/exit 1))))
