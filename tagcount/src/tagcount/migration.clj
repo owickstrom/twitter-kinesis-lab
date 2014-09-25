e9886b2000528a640a986e59286bbf22dbcd53bc
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2014-09-25 17:51:48 +0200

    table-exists? now returns boolean instead of sequence of boolean.

--- tagcount/src/tagcount/migration.clj
+++ tagcount/src/tagcount/migration.clj
@@ -3,25 +3,26 @@
             [tagcount.core :as core]))
 
 (defn table-exists? [name]
-  (jdbc/query core/pooled-db-spec
-              ["select count(*) 
-               from information_schema.tables 
-               where table_schema = 'public'
-               and table_name = ?" name]
-              :row-fn #(> (:count %) 0)))
+  (first
+   (jdbc/query core/pooled-db-spec
+               ["select count(*)
+                from information_schema.tables
+                where table_schema = 'public'
+                and table_name = ?" name]
+               :row-fn #(> (:count %) 0))))
 
-(defn create-tagcount-table []
-  (if (not (table-exists? "tag_count"))
+(defn create-table [table]
+  (if-not (table-exists? table)
     (do
-      (println "Creating table tag_count...")
+      (println "Creating table" table)
       (jdbc/db-do-commands core/pooled-db-spec
                            (jdbc/create-table-ddl
-                             :tag_count
+                             (keyword table)
                              [:tag "varchar(140)" "PRIMARY KEY"]
                              [:count "int" "NOT NULL" "DEFAULT 0"]
                              [:valid_to "timestamptz" "NOT NULL"])))
-    (println "Table tag_count already exists.")))
+    (println "Table" table "already exists.")))
 
 (defn -main []
-  (create-tagcount-table)
+  (create-table "tag_count")
   (println "Done!"))
